665bec2577f33ae79fcf6b336893f6fb5fb6a748
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2024-12-16 23:30:14 +0530

    Nav, Hero, Highlights is completed and in model rotaion is done

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Nav from './Component/Nav'
+import Hero from './Component/Hero'
+import Highlights from './Component/Highlights'
+import Model from './Component/Model'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <main className="bg-black">
+      <Nav/>
+      <Hero/>
+      <Highlights/>
+      <Model />
+
+    </main>
+  )
+}
+
+export default App
